c6cf3351ff3e81cf63087bd221dd98313000413b
Jean Forget <J2N-FORGET@orange.fr>
2013-10-25 07:14:02 +0200

    CPANTS game and kwalitee issues: add a license file and mention it in the code sourcefiles.

--- lib/Date/Convert/French_Rev.pm
+++ lib/Date/Convert/French_Rev.pm
@@ -1,3 +1,9 @@
+#
+#     Perl Date::Convert extension to convert dates from/to the French Revolutionary calendar
+#     Copyright (C) 2001-2003, 2013 Jean Forget
+#
+#     See the license in the embedded documentation below.
+#
 package Date::Convert::French_Rev;
 
 use strict;
@@ -700,10 +706,33 @@ The French Revolution, Thomas Carlyle, Oxford University Press
 
 http://www.faqs.org/faqs/calendars/faq/part3/
 
-=head1 LICENSE STUFF
+=head1 LICENSE
 
-Copyright  (c)  2001, 2002  Jean  Forget.  All  rights reserved.  This
-program is  free software.  You can distribute,  modify, and otherwise
-mangle Date::Convert::French_Rev under the same terms as perl.
+Copyright  (c)  2001,  2002,  2003,  2013  Jean  Forget.   All  rights
+reserved.  This program is free software.  You can distribute, modify,
+and otherwise mangle Date::Convert::French_Rev under the same terms as
+Perl 5.16.3: GNU  Public License version 1 or  later and Perl Artistic
+License
+
+You can  find the text  of the licenses  in the F<LICENSE> file  or at
+L<http://www.perlfoundation.org/artistic_license_1_0> and
+L<http://www.gnu.org/licenses/gpl-1.0.html>.
+
+Here is the summary of GPL:
+
+This program is  free software; you can redistribute  it and/or modify
+it under the  terms of the GNU General Public  License as published by
+the Free  Software Foundation; either  version 1, or (at  your option)
+any later version.
+
+This program  is distributed in the  hope that it will  be useful, but
+WITHOUT   ANY  WARRANTY;   without  even   the  implied   warranty  of
+MERCHANTABILITY  or FITNESS  FOR A  PARTICULAR PURPOSE.   See  the GNU
+General Public License for more details.
+
+You  should have received  a copy  of the  GNU General  Public License
+along  with  this  program;  if   not,  write  to  the  Free  Software
+Foundation, Inc.,  59 Temple Place, Suite 330,  Boston, MA 02111-1307,
+USA.
 
 =cut
