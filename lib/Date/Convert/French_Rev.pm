3582ad3bbd45b18ce01657dc06eec06fa2a8e599
Jean Forget <JFORGET@cpan.org>
2020-09-22 07:01:49 +0200

    New version number.

--- lib/Date/Convert/French_Rev.pm
+++ lib/Date/Convert/French_Rev.pm
@@ -22,7 +22,7 @@ require Exporter;
 @EXPORT = qw(
 
 );
-$VERSION = '0.09';
+$VERSION = '0.10';
 
 use constant REV_BEGINNING => 2375840; # 1 Vendémiaire I in the Revolutionary calendar
 my @MONTHS_SHORT  = qw ( Vnd Bru Fri Niv Plu Vnt Ger Flo Pra Mes The Fru S-C);
